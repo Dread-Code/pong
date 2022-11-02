33828561915b922a57a05aff7f0df107af109aa4
Dread-Code <lucasospina97@gmail.com>
2022-11-02 09:24:30 -0500

    AABB collision for ball and paddle added

--- Ball.lua
+++ Ball.lua
@@ -21,6 +21,37 @@ function Ball:init(x, y, width, height)
     self.dy = math.random(-50, 50)
 end
 
+--[[
+    This is created based of an algorithm call AABB Collision detection
+    you could find a little example of how works here:
+    https://tutorialedge.net/gamedev/aabb-collision-detection-tutorial/
+]]
+function Ball:collides(paddle)
+
+    --[[
+        if you want know more about this lines of code check the following
+        image https://user-images.githubusercontent.com/60331479/199513816-b6f37f62-ea39-47b4-98ba-80e9527e6a3a.png
+    ]]
+
+    --[[
+        First, check to see if the left edge of either is farther to the right
+        than the right edge of the other
+    ]]
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    --[[
+        Then check to see if the bottom edge of either is higher than the top
+        edge of the other
+    ]]
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end
+
+    return true
+end
+
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 -2
